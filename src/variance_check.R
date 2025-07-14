4397a11295087c8f69ab2aa96ed652413afab704
diogo.burgos <diogoaffonseca@gmail.com>
2025-07-14 12:11:50 -0300

    Changed variance check plot

--- src/variance_check.R
+++ src/variance_check.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(vegan)
 library(ggplot2)
 library(ggpubr)
 
@@ -28,10 +29,14 @@ plot <- ggplot(distances_df, aes(x = Group, y = Distance,)) +
         axis.text.y = element_text(size = 12),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        plot.title = element_text(size = 16, face = "bold"))
-
+        plot.title = element_blank())
+#Write the anova results in the plot 
+plot <- plot + 
+  annotate("text", x = 1.5, y = max(distances_df$Distance) * 0.9, 
+           label = paste("p =", format.pval(anova$`Pr(>F)`[1], digits = 2)), 
+           size = 5, hjust = 0.5)
 # Save the plot
-ggsave("results/figures/variance_check.png", plot, width = 8, height = 6, dpi = 300)
+ggsave("results/figures/variance_check.svg", plot, width = 8, height = 6, dpi = 300)
 
 # save the anova results in txt file
 writeLines(capture.output(print(anova)), "results/variance_check.txt")
